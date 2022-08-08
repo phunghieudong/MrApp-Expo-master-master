4c4e6520540866a9037e9436079d9ef173d78053
phunghieudong <phunghieudong1881999@gmail.com>
2022-08-08 15:10:41 +0700

    add file ExaminationDepartment

---
+++ src/views/Root/Screen/Home/ExaminationDepartment.tsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class ExaminationDepartment extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>ExaminationDepartment</div>
+    )
+  }
+}
+
+export default ExaminationDepartment
